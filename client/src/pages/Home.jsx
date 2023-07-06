498b8c9c0f1125df7f30c9a0f4ea24ab64d5f475
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-07-06 21:27:07 +0100

    Add files via upload

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
-import { CustomButton } from '../components/CustomButton.jsx';
+import { CustomButton } from '../components';
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -19,16 +19,16 @@ const Home = () => {
         <motion.section className="home" {...slideAnimation('left')}>
           <motion.header {...slideAnimation("down")}>
             <img 
-              src='./logo.png'
+              src='./threejs.png'
               alt="logo"
-              className="w-12 h-12 object-contain"
+              className="w-18 h-18 object-contain"
             />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                DESIGN IT<br className="xl:block hidden" /> YOUR WAY.
               </h1>
             </motion.div>
             <motion.div
@@ -36,7 +36,7 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              <strong>Unleash your imagination</strong>{" "} and define your own style with our brand-new 3D customization tool. Create your unique and exclusive shirt, tailored to your individuality and personal taste.
               </p>
 
               <CustomButton 
