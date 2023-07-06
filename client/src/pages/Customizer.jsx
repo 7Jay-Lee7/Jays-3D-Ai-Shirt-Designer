060ea34f3f446f6de4d3c85e468d536c195a5b9a
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-07-06 21:08:37 +0100

    Update Customizer.jsx
    Updating link to backend

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://ai-designer.onrender.com/', {
+      const response = await fetch('https://ai-designer.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
